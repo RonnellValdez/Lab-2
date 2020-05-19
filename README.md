3ed965a12b33d1ccc3cbbbe87e0abb58ab8ad7e8
RonnellValdez <ronnell.b.valdez@gmail.com>
2020-05-18 17:54:03 -0700

    Iniitial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lab-2
 ASCII art
+I made a house using ASCII.
---
+++ lab 2.cpp
@@ -0,0 +1,22 @@
+// lab 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    cout << "       ___| |\n"; 
+	cout << "     /    | |\\\n" ;
+	cout << "    /     | | \\ \n";
+	cout << "   /    _ | |  \\\n";
+	cout << "   |   |_|     |  \n";
+	cout << "   |           |\n";
+	cout << "   |     _     |\n";
+	cout << "   |    | |    |\n";
+	cout << "   |____|_|____|\n";
+	cout << "     Ronnell V.\n";
+}
